a3ce3cab6a6fcb7c557bcee4da00a79839a69570
Massimo Ercolani <massimo.ercolani28@gmail.com>
2022-06-29 00:12:37 +0200

    refactor: implement storybook csf

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,13 @@
-const path = require('path');
-
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        // Optional
-        {
-          loader: require.resolve('react-docgen-typescript-loader'),
-          options: {
-            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-          },
-        },
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
   },
 };
---
+++ src/stories/ReactDateInputs.stories.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import DateInputs from '../index';
+import './styles.css';
+
+export default {
+  title: 'React Date Inputs',
+  component: DateInputs,
+} as ComponentMeta<typeof DateInputs>;
+
+const Template: ComponentStory<typeof DateInputs> = (args) => {
+  const [value, setValue] = useState<Date | undefined>(args.value);
+
+  return <DateInputs {...args} value={value} onChange={setValue} />;
+};
+
+export const Simple = Template.bind({});
+Simple.argTypes = {
+  show: {
+    options: ['day', 'month', 'year'],
+    control: { type: 'check' },
+  },
+  dayPlaceholder: {
+    control: { type: 'text' },
+  },
+  monthPlaceholder: {
+    control: { type: 'text' },
+  },
+  yearPlaceholder: {
+    control: { type: 'text' },
+  },
+};
+Simple.args = {
+  label: 'Date',
+  disabled: false,
+  show: ['day', 'month', 'year'],
+  autoTab: false,
+  dayPlaceholder: 'DD',
+  monthPlaceholder: 'MM',
+  yearPlaceholder: 'YYYY',
+};
+
+export const PrePopulated = Template.bind({});
+PrePopulated.argTypes = { ...Simple.argTypes };
+PrePopulated.args = {
+  ...Simple.args,
+  value: new Date(2022, 6, 20),
+};
+
+export const WithCustomComponents = Template.bind({});
+
+const InputComponent = React.forwardRef<HTMLInputElement, React.HTMLProps<HTMLInputElement>>(
+  (props, ref) => {
+    return <input style={props.style} {...props} ref={ref} />;
+  }
+);
+
+InputComponent.displayName = 'InputComponent';
+const LabelComponent: React.FC<React.HTMLProps<HTMLLabelElement>> = (props) => {
+  return (
+    <label
+      style={{
+        textTransform: 'uppercase',
+        fontFamily: 'sans-serif',
+        fontWeight: 'bold',
+        fontSize: '0.8rem',
+      }}
+      {...props}
+    />
+  );
+};
+
+WithCustomComponents.argTypes = {
+  ...Simple.argTypes,
+  inputComponent: {
+    table: { disable: true },
+  },
+  labelComponent: {
+    table: { disable: true },
+  },
+};
+WithCustomComponents.args = {
+  ...Simple.args,
+  inputComponent: InputComponent,
+  labelComponent: LabelComponent,
+  inputComponentProps: {
+    style: {
+      padding: '6px',
+      marginRight: '4px',
+      marginTop: '0.2rem',
+    },
+  },
+};
